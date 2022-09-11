cc5f98fc2df7088302f59352563e51d79f9e928d
Olivia Onyinye Nwachukwu <63042394+Olivia-onyinye@users.noreply.github.com>
2022-09-11 15:09:13 +0100

    Update challenge-1.js
    Challenge 1 solved

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,12 @@
 function isAnagram(str1, str2) {
- 
+ // return str1.split("").sort().join("") === str2.split("").sort().join("");
+   if (str1.length !== str2.length) {
+    return false;
+  }
+  //Sort the two strings
+  var s1 = str1.split("").sort().join("");
+  var s2 = str2.split("").sort().join("");
+  //Compare the two sorted strings
+  return s1 === s2;
 }
 module.exports = isAnagram
