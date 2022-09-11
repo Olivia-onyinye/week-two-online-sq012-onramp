3b91c426f1adc1a0b6c8288235ca466403ba9549
Olivia Onyinye Nwachukwu <63042394+Olivia-onyinye@users.noreply.github.com>
2022-09-11 15:50:14 +0100

    Update challenge-3.js

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,16 @@
-function missingNumber(numArr) {}
-
+function missingNumber(numArr) {
+     let missingArr = [];    
+  if (numArr.length === 9) {
+      return false;
+    }  
+    for (let i = 1; i <= 9; i++) {
+      if (!numArr.includes(i)) {
+        missingArr.push(i);
+      }
+    }
+    if (missingArr.length === 1) {
+      return missingArr[0];
+    }
+    return missingArr;
+}
 module.exports = missingNumber
