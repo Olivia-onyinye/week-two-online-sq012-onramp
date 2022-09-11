4fc2c5bc64f5c58f3c6e79535e05b3993e6cbae3
Olivia Onyinye Nwachukwu <63042394+Olivia-onyinye@users.noreply.github.com>
2022-09-11 16:11:44 +0100

    Update challenge-5.js

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,10 @@
 function isPalindrome(line) {
-
-  return true;
+let str = line.toString();
+    let reverseString = str.split("").reverse().join("");
+    if (str === reverseString) {
+      return true;
+    } else {
+      return false;
+    }
 }
 module.exports = isPalindrome
