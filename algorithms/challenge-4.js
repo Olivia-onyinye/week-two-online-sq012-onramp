1c51323b776204538f1f3a2b8699d648495501ae
Olivia Onyinye Nwachukwu <63042394+Olivia-onyinye@users.noreply.github.com>
2022-09-11 15:55:55 +0100

    Update challenge-4.js
    Solution

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,5 +1,12 @@
 function replaceDivisible(x, y) {
-    
-}
+ let newArr = []
 
+      for (let i = 0; i < x.length; i++){
+        if (x[i] % y === 0){
+            x[i] = "isDivisible";
+        }
+        newArr.push(x[i]);
+      } 
+     return newArr;   
+}
 module.exports = replaceDivisible
